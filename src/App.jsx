058a624b58590afe8b696deeaa168a1c2180bc47
Matias <mgomezg1042@gmail.com>
2026-08-28 21:51:49 -0500

    votación + duelos (lógica con datos falsos)

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Login from "./paginas/Login";
 import Registro from "./paginas/Registro";
 import Perfil from "./paginas/Perfil";
 import Paradojas from "./paginas/Paradojas";
+import DebateParadoja from "./paginas/DebateParadoja";
+import Ranking from "./paginas/Ranking";
 import "./App.css";
 
 function App() {
@@ -17,6 +19,8 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/registro" element={<Registro />} />
           <Route path="/perfil" element={<Perfil />} />
+          <Route path="/debate/:id" element={<DebateParadoja />} />
+          <Route path="/ranking" element={<Ranking />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
